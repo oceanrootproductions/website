c37f47270d98eaee0fb1c71d9f9f1b5b0045e98e
MichelDeveloper <micheldeveloper300@gmail.com>
2025-08-30 21:28:24 +0100

    bugfix router

--- src/main.tsx
+++ src/main.tsx
@@ -21,11 +21,10 @@ const router = createBrowserRouter([
       { path: '*', element: <NotFound /> },
     ],
   },
-])
+], { basename: import.meta.env.BASE_URL })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
-
